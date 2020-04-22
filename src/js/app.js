235210bd42db169abc172f879ae5be29eac9bb50
Adithya Sreyaj <adi.sreyaj@gmail.com>
2020-04-22 22:02:21 +0530

    💫  Adding animations

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,32 @@
-window.addEventListener('resize', () => {
+/*
+ * File: app.js
+ * Project: adithya-portfolio
+ * File Created: Tuesday, 21st April 2020 10:17:07 pm
+ * Author: Adithya Sreyaj
+ * -----
+ * Last Modified: Wednesday, 22nd April 2020 9:14:45 pm
+ * Modified By: Adithya Sreyaj<adi.sreyaj@gmail.com>
+ * -----
+ */
+
+
+window.addEventListener("resize", () => {
   const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
 });
 
 window.toggleMenu = () => {
-  const menu = document.querySelector('.menu');
-  menu.classList.toggle('active');
+  const menu = document.querySelector(".menu");
+  menu.classList.toggle("active");
 };
 
-const scrollTop = document.querySelector('.scroll-top');
+const scrollTop = document.querySelector(".scroll-top");
 
-document.addEventListener('scroll', () => {
+document.addEventListener("scroll", () => {
   const showScrollAt = 500;
   if (window.pageYOffset > showScrollAt) {
-    scrollTop.className = 'scroll-top active';
+    scrollTop.className = "scroll-top active";
   } else {
-    scrollTop.className = 'scroll-top';
+    scrollTop.className = "scroll-top";
   }
 });
